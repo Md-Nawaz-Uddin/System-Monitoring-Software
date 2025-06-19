6b4806d2f3385cd8c6fc07e9d5380aaf2870a6a1
Md Nawaz uddin <159884734+Md-Nawaz-Uddin@users.noreply.github.com>
2025-06-19 14:19:34 +0530

    Create App.js

---
+++ dashboard/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <div>
+      {loggedIn ? <Dashboard /> : <Login setLoggedIn={setLoggedIn} />}
+    </div>
+  );
+}
+
+export default App;
