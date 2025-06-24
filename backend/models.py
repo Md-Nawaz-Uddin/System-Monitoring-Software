daa316bb37e2b17810ff9c28680e155b91f73fd1
Md Nawaz uddin <159884734+Md-Nawaz-Uddin@users.noreply.github.com>
2025-06-24 17:23:11 +0530

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -25,3 +25,19 @@ class CommandLog(db.Model):
     action = db.Column(db.String(200))
     device = db.Column(db.String(100))
     time = db.Column(db.DateTime, default=datetime.utcnow)
+
+class ExtensionPolicy(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    device_id = db.Column(db.String(200))  # hostname or unique ID
+    ext_type = db.Column(db.String(50))    # 'vscode' or 'browser'
+    name = db.Column(db.String(200))       # Extension name
+    mode = db.Column(db.String, nullable=False, default='whitelist')
+    __table_args__ = (
+        db.UniqueConstraint('device_id', 'ext_type', 'name', name='uq_device_ext_policy'),)
+
+    def to_dict(self):
+        return {
+            "device_id": self.device_id,
+            "extension_name": self.extension_name,
+            "extension_type": self.extension_type
+        }
